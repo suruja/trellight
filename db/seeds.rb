0df74257373872791e85b0888c1bf9e026621e30
Sebastien Azimi <sebstp@gmail.com>
2019-04-01 01:48:18 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,39 @@
 column1 = Column.create(
   title: "À rencontrer",
-  index: 0,
+  position: 0,
 )
 
 column2 = Column.create(
   title: "Entretien",
-  index: 1,
+  position: 1,
 )
 
 card1 = Card.create(
-  id: 1,
   title: "Alpha",
-  index: 0,
+  position: 0,
   column: column1,
 )
 
 card2 = Card.create(
-  id: 2,
-  title: "Tango",
-  index: 1,
+  title: "Bravo",
+  position: 1,
   column: column1,
 )
 
 card3 = Card.create(
-  id: 3,
   title: "Charlie",
-  index: 0,
+  position: 0,
   column: column2,
 )
 
 card4 = Card.create(
-  id: 4,
-  title: "Bravo",
-  index: 1,
+  title: "Delta",
+  position: 1,
   column: column2,
 )
 
 card5 = Card.create(
-  id: 5,
-  title: "Marshal",
-  index: 2,
+  title: "Echo",
+  position: 2,
   column: column1,
 )
