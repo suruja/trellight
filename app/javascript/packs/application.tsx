51e14adf8ed36465db8dacf58a9d355d1188d09f
Sebastien Azimi <sebstp@gmail.com>
2019-04-01 15:26:14 +0200

    replace cablecar with actioncable script

--- app/javascript/packs/application.tsx
+++ app/javascript/packs/application.tsx
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import cablecar from 'redux-cablecar'
+import ActionCable from 'actioncable'
 
 import reducer from '../reducers/index'
 import { StoreState } from '../types/index'
@@ -15,18 +15,21 @@ import Dashboard from '../containers/Dashboard'
 
 import 'bulma/css/bulma.css'
 
+const middlewares = [
+  thunk,
+  (process.env.NODE_ENV !== 'production') && logger,
+].filter(Boolean);
+
 const store = createStore<StoreState, Action, any, any>(reducer, {
   cards: {},
   columns: {},
-}, applyMiddleware(
-  thunk,
-  cablecar,
-  logger,
-))
+}, applyMiddleware(...middlewares))
 
-cablecar.connect(store, 'DashboardChannel', {
-  params: { },
-  prefix: SERVER_ACTION,
+const consumer = ActionCable.createConsumer()
+consumer.subscriptions.create({ channel: "DashboardChannel" }, {
+  received(data) {
+    store.dispatch(data)
+  }
 })
 
 document.addEventListener('DOMContentLoaded', () => {
